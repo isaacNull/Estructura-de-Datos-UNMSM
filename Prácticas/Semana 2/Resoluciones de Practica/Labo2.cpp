e08608753da99b0cdcdd94e173e42188e8660310
German <german@Germans-Mac-Pro.local>
2022-05-19 14:09:59 -0500

    añadiendo comentarios al codigo semana 2

--- Prácticas/Semana 2/Resoluciones de Practica/Labo2.cpp
+++ Prácticas/Semana 2/Resoluciones de Practica/Labo2.cpp
@@ -3,9 +3,11 @@
 #include <stdlib.h>
 using namespace std;
 
+// Crea la estructura
 struct Node{
     int valor_node;
     struct Node *next_node;
+    //Constructores
     Node(int valor){
         valor_node = valor;
         next_node = NULL;
@@ -26,6 +28,7 @@ void insertToEnd(TpNode & cabecera);
 void insertToPos(TpNode & cabecera, int pos);
 void showList(TpNode cabecera);
 
+// Main
 int main() {
     TpNode cabecera = new Node(createRandomInt(1,100));
     cout<<"Nodo cabecera con valor " <<cabecera->valor_node<<endl;
@@ -34,6 +37,7 @@ int main() {
     return 0;
 }
 
+// Crea un int random entre 2 limites
 int createRandomInt(int min_value, int max_value){
     /* initialize random seed: */
     srand (time(NULL));
@@ -41,6 +45,7 @@ int createRandomInt(int min_value, int max_value){
 
 }
 
+// Menu
 void menu(TpNode & cabecera){
     int opc;
     cout<<"Elija una opcion: "<<endl;
@@ -72,10 +77,12 @@ void menu(TpNode & cabecera){
     }
 }
 
+// Elige una opcion aleatoria entre 1 y 3 para añadir un nodo
 void insertRandomNode(TpNode& cabecera){
     int insert_mode = createRandomInt(1,3);
     switch (insert_mode) {
         case 1:{
+            cout<<"Agregando nodo al inicio de la lista"<<endl;
             insertToStart(cabecera);
             break;
         }
@@ -87,7 +94,7 @@ void insertRandomNode(TpNode& cabecera){
         default:{
             int pos;
             cout<<"En que posicion desea agregar un nodo?"<<endl;
-            cout<<"Nota:Las posiciones inician de 0)"<<endl;
+            cout<<"Nota:Las posiciones inician de 0"<<endl;
             cout<<"Posicion: ";
             cin>>pos;
             insertToPos(cabecera, pos);
@@ -97,34 +104,36 @@ void insertRandomNode(TpNode& cabecera){
 
 }
 
+// Inserta un nodo al inicio
 void insertToStart(TpNode& cabecera){
-    // Creating new Node;
-    TpNode new_node = new struct Node(createRandomInt(1,100), NULL);
-    new_node->next_node = cabecera;
-    cabecera = new_node;
+    TpNode new_node = new struct Node(createRandomInt(1,100), NULL); // Crea un nodo nuevo
+    new_node->next_node = cabecera; // El puntero del nuevo nodo apunta a la cabecera
+    cabecera = new_node; // cabecera se iguala al nuevo nodo
     cout<<"Agregando nodo con valor "<< new_node->valor_node <<" al inicio de la lista"<<endl;
     showList(cabecera);
 }
 
 
+// Inserta al final
 void insertToEnd(TpNode & cabecera){
     // Creating new Node;
-    TpNode new_node = new Node(createRandomInt(1,100));
-    TpNode temp = cabecera;
+    TpNode new_node = new Node(createRandomInt(1,100)); // Crea un nuevo nodo
+    TpNode cabecera_copy = cabecera; // Variable temporal
     cout<<"Agregando nodo con valor "<< new_node->valor_node <<" al final de la lista"<<endl;
-    if(temp->next_node == NULL){
-        temp->next_node = new_node;
+    if(cabecera_copy->next_node == NULL){ // En caso solo se tenga un nodo en la lista
+        cabecera_copy->next_node = new_node;
         return;
     }
-    while(temp->next_node != NULL){
-        temp = temp->next_node;
+    while(cabecera_copy->next_node != NULL){ // Recorre la lista hasta el ultimo nodo
+        cabecera_copy = cabecera_copy->next_node;
     }
-    temp->next_node = new_node;
+    cabecera_copy->next_node = new_node;
     showList(cabecera);
 }
 
+// Inserta un nodo en una posicion dada
 void insertToPos(TpNode & cabecera, int pos){
-    if(pos == 0){
+    if(pos == 0){ // Cuando la posicion es 0
         insertToStart(cabecera);
         return;
     }
@@ -132,18 +141,20 @@ void insertToPos(TpNode & cabecera, int pos){
     TpNode last_node, cabecera_copy = cabecera, temp_node;
     TpNode new_node = new Node(createRandomInt(1,100));
 
+    // Recorre la lista hasta que se llegue al ultimo elemento O se llegue a la posicion ingresada
     while(cabecera != NULL && pos_cont < pos){
-        last_node = cabecera_copy;
-        if(cabecera_copy->next_node == NULL){
+        last_node = cabecera_copy; // Se guarda el nodo anterior
+        if(cabecera_copy->next_node == NULL){ // Cuando se llega al ultimo elemento
             break;
         }
-        cabecera_copy = cabecera_copy->next_node;
-        pos_cont++;
+        cabecera_copy = cabecera_copy->next_node; // Se avanza en la lista
+        pos_cont++; // Aumenta el contador
     }
-    if(pos_cont == pos){
-        temp_node = cabecera_copy;
-        last_node->next_node = new_node;
-        (last_node->next_node)->next_node = temp_node;
+
+    if(pos_cont == pos){ // Si la lista contiene la posicion dada
+        temp_node = cabecera_copy; // Se guarda en una variable temporal el nodo en la posicion dada
+        last_node->next_node = new_node; // El nodo anterior a la posicion dada apunta al nuevo nodo a insertar
+        (last_node->next_node)->next_node = temp_node; // El nuevo nodo insertado apunta al nodo que antes estaba en su posicion
         cout<<"Agregando nodo con valor "<< new_node->valor_node <<" en la posicion"<<pos<<endl;
     } else {
         cout<<"Posicion no encontrada. Agregando al final"<<endl;
@@ -154,6 +165,7 @@ void insertToPos(TpNode & cabecera, int pos){
 
 }
 
+// Muestra la lista
 void showList(TpNode cabecera){
     int suma_nodes = 0;
     cout<<"Mostrando lista"<<endl;
